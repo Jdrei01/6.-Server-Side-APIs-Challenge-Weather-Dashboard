46cc0fd20a3e6340e137eac379dd1cbe932a5408
Jandrei Timoteo <jandreitimoteo@Jandreis-MacBook-Pro.local>
2023-09-27 20:49:57 -0700

    Added submit, clear button and weather class section

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 var apiKey = '8cd6c39495c0df975b0917be4909b26d';
 var apiUrl = 'https://openweathermap.org/';
+var searchBar = document.querySelector('.search-bar');
 
+
+/*document.querySelector('.search').addEventListener('submit', function (event) {
+    event.preventDefault();
+*/
