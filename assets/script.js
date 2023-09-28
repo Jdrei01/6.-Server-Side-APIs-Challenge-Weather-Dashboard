ce03304428ae823d57c8910036cddefe6766b0b9
Jandrei Timoteo <jandreitimoteo@Jandreis-MacBook-Pro.local>
2023-09-27 22:09:52 -0700

    Added fetch

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,27 @@ var apiKey = '8cd6c39495c0df975b0917be4909b26d';
 var apiUrl = 'https://openweathermap.org/';
 var searchBar = document.querySelector('.search-bar');
 
-
-/*document.querySelector('.search').addEventListener('submit', function (event) {
+document.querySelector('.searchCity').addEventListener('submit', function (event) {
     event.preventDefault();
-*/
+
+    var city = searchBar.value.trim();
+    var catUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
+
+// fetch
+    fetch(catUrl)
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (city) {
+            console.log(city);
+            var lon = city.coord.lon;
+            var lat = city.coord.lat;
+            console.log(lon, lat);
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log('hello')
+                })
+        })
+});
